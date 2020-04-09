2f71f8e0926429b4274d15aa5fea49ca303978d3
satyam9753 <satyam.anand9753@gmail.com>
2020-04-10 04:25:48 +0530

    encryption of files done

--- encrypting-password.py
+++ encrypting-password.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+#Changing 'user_password' will change the "key" 
 user_password = 'password'
 password = user_password.encode()
 
